d8eb53a54f054a27073ae508ebf76b4e167e5358
Krishna Nigalye <krishna.nigalye@affiliate.ansr.com>
2021-09-28 13:21:07 +0530

    added one more exmaple to callbacks

--- callbacks/callbacks.js
+++ callbacks/callbacks.js
@@ -10,3 +10,30 @@ const oddNumbers = numbers.filter((number) => number % 2);
 
 console.log(evenNumbers);
 console.log(oddNumbers);
+
+
+// --------------------
+
+function download(url, callback) {
+  setTimeout(() => {
+    // script to download the picture here
+    console.log(`Downloading ${url} ...`);
+    // process the picture once it is completed
+    callback(url);
+
+  }, 2000);
+}
+
+let url = 'https://cdn.pixabay.com/photo/2018/01/14/23/12/nature-3082832_1280.jpg';
+
+// function to process
+function process(picture) {
+  console.log(`Processing ${picture}`);
+}
+
+download(url, process);
+
+// or pass it as an anonymour function
+// download(url, function(picture) {
+//   console.log(`Processing ${picture}`);
+// }); 
